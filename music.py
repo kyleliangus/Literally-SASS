c13561f009dde67ca9b52501585d2cb8d9c7e73b
kyleliangus <kyleliangus@gmail.com>
2016-11-13 05:08:54 -0800

    Finished internal server

--- music.py
+++ music.py
@@ -1,19 +1,12 @@
 from watson_developer_cloud import SpeechToTextV1, ToneAnalyzerV3
 import json
 
-import os
-import sys
-import spotipy
-import spotipy.util as util
-from spotipy.oauth2 import SpotifyClientCredentials
-
-from flask import Flask, request, redirect, g, render_template
+from flask import Flask, request, redirect, g, render_template, url_for
 import requests
 import base64
 import urllib
 
-
-
+import os
 
 # turn .wav file to text
 def IBMAPI_Calls(f):
@@ -68,7 +61,7 @@ def IBMAPI_Calls(f):
     energy = max((extraversion + joy + emotional_range - tentative )/3, 0)
     instrumentalness = (sadness + tentative) / 2
     liveness = (confident + disgust + extraversion) / 3
-    loudness = (anger + fear) / 2
+    loudness = (1 - (anger + fear) / 2) * (-1) * 60.0
 
     if((sadness + tentative + conscientiousness / 3) > (joy + confident) / 2):
         mode = 0;
@@ -77,14 +70,17 @@ def IBMAPI_Calls(f):
 
     popularity = (extraversion + agreeableness) / 2
     speechiness = max((extraversion + openness - sadness) / 2, 0)
-    tempo = (anger + fear) / 2
+    #tempo = (anger + fear) / 2
     valence = max((joy + confident + extraversion - anger - sadness) / 3, 0)
 	
-	authorization_query = {'target_acousticness':acousticness, 'target_liveness': liveness, 'target_loudness': loudness, 'target_tempo': tempo, 'target_speechiness': speechiness, 'target_instrumentalness': instrumentalness, 'target_valence': valence, 'target_popularity': popularity, 'target_energy': energy, 'target_danceability': danceability, 'target_mode' = mode }
-    return authorization_query
+    authorization_query = {'target_acousticness': acousticness, 'target_liveness': liveness, 'target_loudness': loudness, 'target_speechiness': speechiness, 'target_instrumentalness': instrumentalness, 'target_valence': valence, 'target_popularity': popularity, 'target_energy': energy, 'target_danceability': danceability, 'target_mode': mode }
+    return
 	
 
-# call spotify API
+# call spotify API and start server
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['m4a', 'wav', 'ogg'])
 
 app = Flask(__name__)
 # Client Keys
@@ -107,6 +103,8 @@ STATE = ""
 SHOW_DIALOG_bool = True
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
+authorization_query = {'target_acousticness': 0.5, 'target_liveness': 0.5, 'target_loudness': -30, 'target_speechiness': 0.5, 'target_instrumentalness': 0.5, 'target_valence': 0.5, 'target_popularity': 50, 'target_energy': 0.5, 'target_danceability': 0.5, 'target_mode': 1 }
+
 auth_query_parameters = {
     "response_type": "code",
     "redirect_uri": REDIRECT_URI,
@@ -121,25 +119,27 @@ def index():
     # Auth Step 1: Authorization
     url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
-    return redirect(url_for(auth_url,speech=request.args.get('speech'))
+    #print auth_url
+    return redirect(auth_url)
 
-
-@app.route('/start', methods=['POST'])
+	
+@app.route("/start", methods=['POST'])
 def receive():
     error = None
     f = request.files['speech']
+    os.system(".\ffmpeg.exe " + f.toString() +  )
     if( f ):
-        speech = IBMAPI_Calls( f )
-		return redirect(url_for('', speech=speech))
+        IBMAPI_Calls( f )
+        index()
     else:
-		error = 'No audio file detected'
-		return render_template('index.html', error=error)
+        error = 'No audio file detected'
+        return render_template('index.html', error=error)
 	
     
 @app.route("/callback/q")
 def callback():
     # Auth Step 4: Requests refresh and access tokens
-	authorization_query = request.args.get('speech')
+    # authorization_query = request.args.get('speech')
 	
     auth_token = request.args['code']
     code_payload = {
@@ -163,10 +163,8 @@ def callback():
 	
     genres = {'rap': 0, 'rock': 0, 'pop': 0, 'kpop': 0, 'r&b': 0, 'country': 0, 'latin': 0, 'dance': 0, 'classical': 0, 'jazz': 0}
 	# pick a subset on 4 criteria
-    #genres['r&b'] += popularity * 7.5
-    query = seed_genres.copy()
-	query.update(authorization_query)
-	# pick a subset on 4 criteria
+	
+    popularity = authorization_query['target_popularity']
     # Based off of Popularity:
     genres['r&b'] += popularity / 100 *       .75
     genres['rock'] += popularity / 100 *      .75
@@ -179,6 +177,7 @@ def callback():
     genres['classical'] += popularity / 100 * .25
     genres['jazz'] += popularity / 100 *      .25
 
+    speechiness = authorization_query['target_speechiness']
     # Based off of Speechiness:
     genres['rap'] += speechiness *       .75
     genres['rock'] += speechiness *      .75
@@ -191,6 +190,7 @@ def callback():
     genres['classical'] += speechiness * .25
     genres['jazz'] += speechiness *      .25
 
+    energy = authorization_query['target_energy']
     # Based off of Energy:
     genres['pop'] += energy *       .75
     genres['latin'] += energy *     .75
@@ -203,6 +203,7 @@ def callback():
     genres['country'] += energy *   .25
     genres['jazz'] += energy *      .25
 
+    acousticness = authorization_query['target_acousticness']
     # Based off of Acoustic:
     genres['pop'] += acousticness *       .25
     genres['latin'] += acousticness *     .25
@@ -215,9 +216,13 @@ def callback():
     genres['country'] += acousticness *   .75
     genres['jazz'] += acousticness *      .75
 
-    seed_genres = sorted(genres, key = genres.__getitem__)[:3]
-    print seed_genres
+    seed_genres = sorted(genres, key = genres.__getitem__)[:-4:-1]
     
+    query = authorization_query.copy()
+    query['seed_genres'] = seed_genres
+    query['limit'] = 1;
+    print query
+	
     recommendations_endpoint = "{}/recommendations".format(SPOTIFY_API_URL)
     recommendations_response = requests.get(recommendations_endpoint, params=query, headers=authorization_header)
     recommendations_data = json.loads(recommendations_response.text)
