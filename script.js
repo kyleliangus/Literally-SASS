8dca4e0a50111c94db8b50ef6060110475821257
Emily Gosti <emily.gosti@gmail.com>
2016-11-13 00:44:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#submit").click(function() {
+    var url = "sample-url.php";
+    var params = "lorem=ipsum&name=alpha";
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.send(params);
+  })
+
+})
